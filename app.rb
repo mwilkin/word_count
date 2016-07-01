21fcba7ed01b4c6ed20874178e6cc306beec3fba
Matt Wilkin <mwilkin22@gmail.com>
2016-07-01 15:29:22 -0700

    correct typos in word_count.erb and app.rb. 1 Failure still exists.

--- app.rb
+++ app.rb
@@ -1,17 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/word_count.rb')
-
+require('./lib/word_count')
 also_reload('lib/**/*.rb')
 
 get('/') do
   @page_title = "home"
   erb(:index)
 end
-get('/word_count') do
-  @page_title = "word_count"
-  @input_word = params.fetch('input_word') #NTS: is this necessary?
-  @input_string = params.fetch('input_string')
-  @result = @input_word.word_count(@input_string)
+post('/word_count') do
+  @input_word = params.fetch('word')
+  @input_string = params.fetch('string')
+  @result = @input_string.word_count(@input_string)
   erb(:word_count)
 end
--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -3,19 +3,19 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# describe('word_count path', {:type => :feature}) do
-#   it('takes a word and string and determines the number of times the word occurs in the string') do
-#     visit('/word_count')
-#     fill_in('input_word', :with => "dog")
-#     fill_in('input_string', with => "My dog likes to go to the dog park and play with the other dogs.")
-#     click_button('Evaluate')
-#     expect(page).to have_content("occurances: 3")
-#   end
-#   it('takes a word and string and determines the number of times the word occurs in the string with no occurances') do
-#     visit('/word_count')
-#     fill_in('input_word', :with => "dog")
-#     fill_in('input_string', with => "My cat likes to sleep all day.")
-#     click_button('Evaluate')
-#     expect(page).to have_content("occurances: 0")
-#   end
-# end
+describe('word_count', {:type => :feature}) do
+  it('will process the user entry of a word and string and will determine the number of times the word occurs in the string') do
+    visit('/')
+    fill_in('word', :with => "dog")
+    fill_in('string', :with => "My dog likes to go to the dog park and play with the other dogs.")
+    click_button('Evaluate')
+    expect(page).to have_content("occurances: 3")
+  end
+  it('takes a word and string and determines the number of times the word occurs in the string with no occurances') do
+    visit('/')
+    fill_in('word', :with => "dog")
+    fill_in('string', :with => "My cat likes to sleep all day.")
+    click_button('Evaluate')
+    expect(page).to have_content("occurances: 0")
+  end
+end
