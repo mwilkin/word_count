40121e91c7838ae95d03450acc656289ad3fd112
Matt Wilkin <mwilkin22@gmail.com>
2016-07-01 15:53:37 -0700

    6 examples, 0 failures.

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -11,6 +11,13 @@ describe('home page word frequency count', {:type => :feature}) do
     click_button('Evaluate')
     expect(page).to have_content("1")
   end
+  it('will process the user entry of a word and string and will determine the number of times the word occurs in the string') do
+    visit('/')
+    fill_in('word', :with => "dog")
+    fill_in('string', :with => "My cat likes food.")
+    click_button('Evaluate')
+    expect(page).to have_content("0")
+  end
 end
 
 
