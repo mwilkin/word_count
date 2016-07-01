0bf40a9a232aacc47b97a60a4210bab957ce9607
Matt Wilkin <mwilkin22@gmail.com>
2016-07-01 09:47:18 -0700

    fix spelling errors in README. md and word_count.rb.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,8 +1,8 @@
 class String
   define_method(:word_count) do |input_word, input_string|
-     input_string.downcase().split("")
+     input_string_array = input_string.downcase().split("")
 
      frequencies = Hash.new(0)
-     input_string.each{|input_word| frequency[input_sting] +=1}
+     input_string_array.each{|input_word| frequency[input_sting] +=1}
      return frequency
 end
